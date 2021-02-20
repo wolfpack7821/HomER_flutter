2fec9f10e7622357da63782f2eb9523447ed827d
Goutham K <k.goutham2000@gmail.com>
2021-02-20 12:11:42 +0530

    init commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('init commmit'),
+      child: Text('init commit'),
     );
   }
 }
